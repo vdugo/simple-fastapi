b38e6bcbe68347a8099a03c9471a51463c51f7b5
unknown <v3rdugo@yahoo.com>
2022-07-08 16:22:08 -0700

    set up Pydantic models according to OpenAPI specification

--- schemas.py
+++ schemas.py
@@ -1,4 +1,33 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+from uuid import UUID
+
 from pydantic import BaseModel
 
 class Error(BaseModel):
-    detail: str
+    detail: Optional[str] = None
+
+class Priority(Enum):
+    low = 'low'
+    medium = 'medium'
+    high = 'high'
+
+class Status(Enum):
+    progress = 'progress'
+    pending = 'pending'
+    completed = 'completed'
+
+class CreateTaskSchema(BaseModel):
+    priority: Optional[Priority] = 'low'
+    status = Optional[Status] = 'pending'
+
+class GetTaskSchema(BaseModel):
+    id: UUID
+    created: datetime
+    priority: Priority
+    status: Status
+    task: str
+
+class ListTasksSchema(BaseModel):
+    tasks: List[GetTaskSchema]
